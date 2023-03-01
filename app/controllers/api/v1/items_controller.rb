7be2a48385797061845eeb15924e7c10107abc93
smy-101 <shimy1011@foxmail.com>
2023-03-01 22:16:16 +0800

    创建api

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::ItemsController < ApplicationController
+  def create
+    item = Item.new amount: 1
+    if item.save
+      render json: { resource: item }
+    else
+      render json: { errors: item.errors }
+    end
+  end
+end
