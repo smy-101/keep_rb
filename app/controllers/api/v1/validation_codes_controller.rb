16f87b43e34051ccb9a03d571c4b047706f20423
smy-101 <shimy1011@foxmail.com>
2023-03-01 22:34:50 +0800

    引入分页gem

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::ValidationCodesController < ApplicationController
+  def index
+    items = Item.page(params[:page]).per(100)
+    render json: { resources: items, pager: {
+      page: params[:page],
+      per_page: 100,
+      count: Item.count
+    }}
+  end
   def create
-    head 201
+    item = Item.new amount: 1
+    if item.save
+      render json: {resource: item}
+    else
+      render json: {errors: item.errors}
+    end
   end
 end
